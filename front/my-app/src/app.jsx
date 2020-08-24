ce056743b42d7e62f7f22ef1be521500f2ae456f
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-08-24 18:28:44 -0300

    kid of buttons

--- front/my-app/src/app.jsx
+++ front/my-app/src/app.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import {NomeDaClasse} from './style'
-function App(){
+import { NomeDaClasse, Button, Button2 } from './style'
+function App() {
 
-    const counter =()=>{
-        
-console.log("click here")
+    const counter = () => {
+
+        console.log("click here")
     }
-    
+
     return (
         <div>
-        <NomeDaClasse>
-            <p>Teste</p>
-            <button onClick={()=>{counter()}}><span>click here</span></button>
-        </NomeDaClasse>
+            <NomeDaClasse>
+                <p>Teste</p>
+                <button onClick={() => { counter() }}><span>click here</span></button>
+            </NomeDaClasse>
+            <Button> OK </Button>
+            <Button2>OK2</Button2>
         </div>
     )
 }
--- front/my-app/src/style.js
+++ front/my-app/src/style.js
@@ -1,11 +1,79 @@
 import styled, { keyframes, css } from 'styled-components';
 export const NomeDaClasse = styled.div`
- background-color: #4CAF50; /* Green */
+  button {
+    position: relative;
+  background-color: #4CAF50;
   border: none;
-  color: white;
-  padding: 15px 32px;
+  font-size: 28px;
+  color: #FFFFFF;
+  padding: 20px;
+  width: 200px;
   text-align: center;
+  transition-duration: 0.4s;
   text-decoration: none;
-  display: inline-block;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+button:after {
+  content: "";
+  background: #f1f1f1;
+  display: block;
+  position: absolute;
+  padding-top: 300%;
+  padding-left: 350%;
+  margin-left: -20px !important;
+  margin-top: -120%;
+  opacity: 0;
+  transition: all 0.8s
+}
+button:active:after {
+  padding: 0;
+  margin: 0;
+  opacity: 1;
+  transition: 0s
+}
 `;
 
+
+
+export const Button = styled.button`
+background-color: #f4511e;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  font-size: 16px;
+  margin: 4px 2px;
+  opacity: 0.6;
+  transition: 0.3s;
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+
+  :hover {opacity: 1}
+`;
+
+
+
+export const Button2 = styled.button`
+display: inline-block;
+  padding: 15px 25px;
+  font-size: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+
+  :hover {background-color: #3e8e41}
+  :active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+`;
